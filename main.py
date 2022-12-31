f3126552d9bf85457ba2c40c2d6b11dec5f67241
Yash Kumar <72352678+kyash108@users.noreply.github.com>
2022-12-31 02:38:45 +0000

    Pizza order billing

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 # 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
-if year%4==0:
-    if year%100!=0:
-        print("Leap year.")
-    else:
-        if year%400==0:
-            print("Leap year.")
+total = 0
+if size == "S":
+    total += 15
+    if add_pepperoni == "Y":
+        total += 2
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
         else:
-            print("Not leap year.")
-else:
-    print("Not leap year.")
+            print(f"Your final bill is: ${total}.")
+    else:
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
+        else:
+            print(f"Your final bill is: ${total}.")
+elif size == "M":
+    total += 20
+    if add_pepperoni == "Y":
+        total += 3
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
+        else:
+            print(f"Your final bill is: ${total}.")
+    else:
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
+        else:
+            print(f"Your final bill is: ${total}.")
+elif size == "L":
+    total += 25
+    if add_pepperoni == "Y":
+        total += 3
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
+        else:
+            print(f"Your final bill is: ${total}.")
+    else:
+        if extra_cheese == "Y":
+            total +=1
+            print(f"Your final bill is: ${total}.")
+        else:
+            print(f"Your final bill is: ${total}.")
 

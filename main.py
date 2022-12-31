707cb142daa04f2d90e658cc74254f91ae4058ba
Yash Kumar <72352678+kyash108@users.noreply.github.com>
2022-12-31 02:37:29 +0000

    BMI calculator

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-firstCheck = input("Which way would you like to go? ")
-if firstCheck.lower() == "left":
-    secondCheck = input("Would you like to swim or wait? ")
-    if secondCheck.lower() == "wait":
-        thirdCheck = input("Which door would you like to open? Blue, Red, or Yellow? ")
-        if thirdCheck.lower() == "red" or thirdCheck.lower() == "blue":
-            print("Game over.")
-        elif thirdCheck.lower() == "yellow":
-            print("You win!")
-    else:
-        print("Game over.")
+bmi = round(weight/(height*height))
+if bmi<18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi>18.5 and bmi<25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi>25 and bmi<30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi>30 and bmi<35:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi>35:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
 else:
-    print("Game over.")
+    print("all working")
